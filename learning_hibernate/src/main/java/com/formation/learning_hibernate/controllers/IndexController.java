6ac912dfe5f7a18906d3d411816aee19a7f95e7f
Vincent  Fauré <47210589+Vince-Faure@users.noreply.github.com>
2023-07-24 17:04:16 +0200

    end of the day

--- learning_hibernate/src/main/java/com/formation/learning_hibernate/controllers/IndexController.java
+++ learning_hibernate/src/main/java/com/formation/learning_hibernate/controllers/IndexController.java
@@ -5,7 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RequestParam;
 
 import com.formation.learning_hibernate.entities.ContactEntity;
 
@@ -19,18 +19,16 @@ public class IndexController {
         model.addAttribute("title","Learning Hibernate");
         return "page.html";
     }
+
     @GetMapping("/contact")
     public String contact(Model model) {
         model.addAttribute("contact",new ContactEntity());        
         return "contact/form.html";
      }
+
     @PostMapping("/contact")
     public String contactSend(@ModelAttribute(name="contact") ContactEntity contact) {
-       
-          
         System.out.println(contact);
-
-
         return "contact/form.html";
      }
     }
--- learning_hibernate/src/main/java/com/formation/learning_hibernate/entities/ContactEntity.java
+++ learning_hibernate/src/main/java/com/formation/learning_hibernate/entities/ContactEntity.java
@@ -1,5 +1,10 @@
 package com.formation.learning_hibernate.entities;
 
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Data
@@ -8,8 +13,18 @@ import lombok.*;
 public class ContactEntity {
     
     private Integer id;
+
+    @NotBlank
+    @Size(min= 3, max=30)
+    @Pattern(regexp="^[\\p{L}\\- ]{4,100}$", flags ={Pattern.Flag.CASE_INSENSITIVE,Pattern.Flag.DOTALL})
     private String name;
+
+    @NotBlank
+    @Email
     private String email;
+
+    @NotBlank
+    @Size(min=25, max=1000)
     private String message;
 
 }
